b29c2c17ca56245c6108a469514f7845ba07d580
Sean Richardson <Sean.r2000@hotmail.com>
2016-01-20 16:55:43 +0000

    Added Manhattan Distance Heuristic Calculation
    code which works out the h(node) part of the A* algorithm

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Game : MonoBehaviour {
+
+    public List<Tile> openList;
+    List<Tile> closeList;
+
+	// Use this for initialization
+	void Start () 
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        //calculate Manhattan Distance
+        foreach(Tile tile in openList)
+        {
+            if(tile.targetPos.x != tile.transform.position.x)
+            {
+                tile.heuristicValue += Mathf.Abs(tile.targetPos.x - tile.transform.position.x);
+            }
+            if(tile.targetPos.y != tile.transform.position.y)
+            {
+                tile.heuristicValue += Mathf.Abs(tile.targetPos.y - tile.transform.position.y);
+            }
+            else
+            {
+                tile.heuristicValue = 0;
+            }
+            
+        }
+	}
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour 
+{
+    public Vector2 targetPos;
+
+    public float heuristicValue;
+    Game game;
+
+	// Use this for initialization
+    void Start()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+	}
+}
