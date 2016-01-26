5d9fb067b141ce8a4429b1e517c9ca85b85e3925
SeanMRichardson <Sean.r2000@hotmail.com>
2016-01-26 16:26:22 +0000

    fixed Manhattan Distance calculation
    heuristics now work fine

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -9,6 +9,8 @@ public class Game : MonoBehaviour
 	Tile[] tiles;
 	public GameObject tile;
 
+	public GameObject obj;
+
 	public List<Tile> gameBoard = new List<Tile>();
 	List<Tile> closeList;
 
@@ -20,12 +22,14 @@ public class Game : MonoBehaviour
 
 	public float h;
 
+	bool boardSpawned = false;
+
 	void Start() 
 	{
 		// intialize the board to the end position
-		tile.GetComponent<Tile>().id = 0;
+		tile.GetComponent<Tile>().id = 1;
 		text = tile.transform.FindChild("Text");
-		for (int j = 2; j >= 0; --j)
+		for (int j = 3; j >= 1; --j)
 		{
 			for (int i = 0; i < 3; ++i)
 			{
@@ -33,26 +37,27 @@ public class Game : MonoBehaviour
 				tile.GetComponent<Tile>().targetPos.x = i;
 				tile.GetComponent<Tile>().targetPos.y = j;
 
-				// set the text on the tiles and instantiate
-				tile.GetComponent<Tile>().id++;
 				tileId = tile.GetComponent<Tile>().id;
-				text.GetComponent<Text>().text = tileId.ToString();
+				// set the text on the tiles and instantiate
 				if (tileId <= puzzleType)
 				{
-					Instantiate(tile, tile.GetComponent<Tile>().targetPos, Quaternion.identity);
-					gameBoard.Add(tile.GetComponent<Tile>());
+					text.GetComponent<Text>().text = tileId.ToString();
 					
+					// instantiate a gameobject to the list
+					obj = Instantiate(tile, tile.GetComponent<Tile>().targetPos, Quaternion.identity) as GameObject;
+					gameBoard.Add(obj.GetComponent<Tile>());
+					tile.GetComponent<Tile>().id++;
 				}
-				
 			}
 		}		
 	}
 
-	// Update is called once per frame
 	void Update()
 	{
+		
 		foreach (Tile tile in gameBoard)
 		{
+			h = 0;
 			// get the heuristic value for the individual tiles
 			CalculateManhattanDistance(tile);
 
@@ -63,17 +68,20 @@ public class Game : MonoBehaviour
 
 	void CalculateManhattanDistance(Tile tile)
 	{
+		float hValue = 0;
 		// check how far away from the target location the tile is
 		if (tile.currentPos.x != tile.targetPos.x)
 		{
 			// set the tiles heuristic value
-			tile.heuristicValue += Mathf.Abs(tile.targetPos.x - tile.currentPos.x);
+			hValue += Mathf.Abs(tile.targetPos.x - tile.currentPos.x);
+			tile.heuristicValue = hValue;
 		}
 		if (tile.currentPos.y != tile.targetPos.y)
 		{
-			tile.heuristicValue += Mathf.Abs(tile.targetPos.y - tile.currentPos.y);
+			hValue += Mathf.Abs(tile.targetPos.y - tile.currentPos.y);
+			tile.heuristicValue = hValue;
 		}
-		else
+		if(tile.currentPos == tile.targetPos)
 		{
 			tile.heuristicValue = 0;
 		}
